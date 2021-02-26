80d59f8e37caebdea0af7ec8c90950f6cd3c900f
Victoria <victoriarsaliba@gmail.com>
2021-02-26 15:02:43 -0300

    Refatorando o Teste Funcional

--- tdd-project/functional_tests.py
+++ tdd-project/functional_tests.py
@@ -59,3 +59,8 @@ class NewVisitorTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+def check_for_row_in_list_table(self, row_text):
+   table = self.browser.find_element_by_id('id_list_table')
+   rows = table.find_elements_by_tag_name('tr')
+   self.assertIn(row_text, [row.text for row in rows])
