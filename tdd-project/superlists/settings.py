a2df2aac6f4543703c56a6588e405d9dd518201b
Victoria <victoriarsaliba@gmail.com>
2021-03-03 18:18:15 -0300

    Explorando autenticação por email (passwordless)

--- tdd-project/superlists/settings.py
+++ tdd-project/superlists/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'accounts.ListUser'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
+]
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'testmenow0505@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
